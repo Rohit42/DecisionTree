a4289aaddbef25e74b6bac94739180820d9b363a
Rohit42 <rohit.mittapalli@gmail.com>
2017-09-26 21:10:46 -0400

    Add files via upload

---
+++ DecisionTree/DecisionTree/DecisionTree/Leaf.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTree
+{
+    public class Leaf
+    {
+        private Leaf output1 = null;
+        private Leaf output2 = null;
+        private double split;
+        private int variable;
+        private double nBackground = 0;
+        private double nSignal = 0;
+
+        public Leaf() :
+            this(0, 0)
+        { }
+
+        public Leaf(int variable, double split)
+        {
+            this.variable = variable;
+            this.split = split;
+        }
+
+        public void WriteToFile(string filename)
+        {
+            var bw = new BinaryWriter(File.Create(filename));
+            write(bw);
+            bw.Close();
+        }
+
+        private void write(BinaryWriter bw)
+        {
+            bw.Write(variable);
+            bw.Write(split);
+
+            bool isfinal = IsFinal();
+            bw.Write(isfinal);
+            if (!isfinal)
+            {
+                output1.write(bw);
+                output2.write(bw);
+            }
+        }
+
+        public Leaf(string filename)
+        {
+            var br = new BinaryReader(File.OpenRead(filename));
+            read(br);
+        }
+
+        private Leaf(BinaryReader sr)
+        {
+            read(sr);
+        }
+
+        private void read(BinaryReader br)
+        {
+            variable = br.ReadInt32();
+            split = br.ReadDouble();
+            bool fin = br.ReadBoolean();
+            if (!fin)
+            {
+                output1 = new Leaf(br);
+                output2 = new Leaf(br);
+            }
+        }
+
+        public bool IsFinal()
+        {
+            return output1 == null || output2 == null;
+        }
+
+        public double GetPurity()
+        {
+            return 1.0*nSignal/ (1.0 * nBackground + nSignal);
+        }
+
+        public double RunDataPoint(DataPoint dataPoint)
+        {
+            if (IsFinal())
+            {
+                return GetPurity();
+            }
+
+            if (doSplit(dataPoint))
+            {
+                return output1.RunDataPoint(dataPoint);
+            }
+            else
+            {
+                return output2.RunDataPoint(dataPoint);
+            }
+        }
+
+        private bool doSplit(DataPoint dataPoint)
+        {
+            return dataPoint.Variables[variable] <= split;
+        }
+
+        public void Train(DataSet signal, DataSet background)
+        {
+            foreach (var p in  signal.Points) {
+                nSignal = nSignal + p.weight;
+            }
+            foreach (var p in background.Points)
+            {
+                nBackground = nBackground + p.weight;
+            }
+            bool branch = chooseVariable(signal, background);
+
+            if (branch)
+            {
+                output1 = new Leaf();
+                output2 = new Leaf();
+
+                DataSet signalLeft = new DataSet();
+                DataSet signalRight = new DataSet();
+                DataSet backgroundLeft = new DataSet();
+                DataSet backgroundRight = new DataSet();
+
+                foreach (var dataPoint in signal.Points)
+                {
+                    if (doSplit(dataPoint))
+                    {
+                        signalLeft.AddDataPoint(dataPoint);
+                    }
+                    else
+                    {
+                        signalRight.AddDataPoint(dataPoint);
+                    }
+                }
+
+                foreach (var dataPoint in background.Points)
+                {
+                    if (doSplit(dataPoint))
+                    {
+                        backgroundLeft.AddDataPoint(dataPoint);
+                    }
+                    else
+                    {
+                        backgroundRight.AddDataPoint(dataPoint);
+                    }
+                }
+
+                output1.Train(signalLeft, backgroundLeft);
+                output2.Train(signalRight, backgroundRight);
+            }
+        }
+
+        private bool chooseVariable(DataSet signal, DataSet background)
+        {
+
+            int bestVar = -1;
+            double bestSplit = -1;
+            double bestPurityDifference = -1;
+            int nVars = signal.Points[0].Variables.Length;
+            for (int i = 0; i < nVars; i++) {
+                double maxSignal = Double.MinValue;
+                double minSignal = Double.MaxValue;
+                double maxBackground = Double.MinValue;
+                double minBackground = Double.MaxValue;
+                foreach (var p in signal.Points) {
+
+                    if (p.Variables[i] > maxSignal) {
+                        maxSignal = p.Variables[i];
+                    }
+                    if (p.Variables[i] < minSignal)
+                    {
+                        minSignal = p.Variables[i];
+                    }
+                }
+                foreach (var p in background.Points)
+                {
+                    if (p.Variables[i] > maxBackground)
+                    {
+                        maxBackground = p.Variables[i];
+                    }
+                    if (p.Variables[i] < minBackground)
+                    {
+                        minBackground = p.Variables[i];
+                    }
+                }
+                double min = Math.Min(minBackground, minSignal);
+                double max = Math.Max(maxBackground, maxSignal);
+
+                double increment = Math.Abs((max - min) / 1000);
+
+
+
+                for (double j = min; j < max; j = j + increment) {
+                    double signalAbove = 0;
+                    double signalBelow = 0;
+                    double backgroundAbove = 0;
+                    double backgroundBelow = 0;
+                    foreach (var p in signal.Points) {
+                        if (p.Variables[i] > j) {
+                            signalAbove = signalAbove + p.weight;
+                        }
+                        if(p.Variables[i] < j) {
+                            signalBelow = signalBelow + p.weight;
+                        }
+                    }
+                    foreach (var p in background.Points)
+                    {
+                        if (p.Variables[i] > j)
+                        {
+                            backgroundAbove = backgroundAbove + p.weight;
+                        }
+                        if(p.Variables[i] < j) {
+                            backgroundBelow = backgroundBelow + p.weight;
+                        }
+                    }
+
+
+                    double purityAbove = signalAbove / (1.0*signalAbove + signalBelow);
+                    double purityBelow = backgroundAbove / (1.0*backgroundBelow + backgroundAbove);
+                    
+
+                    if (Math.Abs(purityAbove - purityBelow) > bestPurityDifference && signalAbove > 50 && signalBelow>50 && backgroundBelow> 50 && backgroundAbove>50)
+                    {
+
+                        bestSplit = j;
+                        bestVar = i;
+                        bestPurityDifference = Math.Abs(purityAbove - purityBelow);
+
+                    }
+
+                }
+
+
+            }
+            if (bestPurityDifference > 0)
+            {
+                split = bestSplit;
+                variable = bestVar;
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ DecisionTree/DecisionTree/DecisionTree/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet ds = new DataSet(@"C:\Users\Rohit\Documents\Visual Studio 2017\Projects\DecisionTree\DecisionTree\DecisionTree\signal.dat");
+            DataSet db = new DataSet(@"C:\Users\Rohit\Documents\Visual Studio 2017\Projects\DecisionTree\DecisionTree\DecisionTree\background.dat");
+            DataSet data = new DataSet(@"C:\Users\Rohit\Documents\Visual Studio 2017\Projects\DecisionTree\DecisionTree\DecisionTree\decisionTreeData.dat");
+            int treeCount = 11;
+            double[] weights = new double[treeCount+1];
+            weights[0] = 1;
+            Leaf[] forest = new Leaf[treeCount];
+            for (int i = 0; i < ds.Points.Count; i++)
+            {
+                ds.Points[i].weight = 1 ;
+
+            }
+            for (int i = 0; i < db.Points.Count; i++)
+            {
+                db.Points[i].weight = 1;
+            }
+            for (int h = 0; h < treeCount; h++)
+            {
+                Console.WriteLine(h);
+                forest[h] = new Leaf();
+                forest[h].Train(ds, db);
+                Console.WriteLine("done training");
+                double counter = 0;
+                for (int i = 0; i < ds.Points.Count; i++)
+                {
+                    double r = forest[h].RunDataPoint(ds.Points[i]);
+                    
+                    if (r < 0.5)
+                    {
+                        counter++;
+
+                    }
+                }
+                for (int i = 0; i < db.Points.Count; i++)
+                {
+                    double r = forest[h].RunDataPoint(db.Points[i]);
+                    if (r > 0.5)
+                    {
+                        counter++;
+                    }
+                }
+                counter = counter / (ds.Points.Count + db.Points.Count);
+                weights[h + 1] = (1-counter) / counter;
+                for (int i = 0; i < ds.Points.Count; i++)
+                {
+                    double r = forest[h].RunDataPoint(ds.Points[i]);
+                    if (r < 0.5)
+                    {
+                        double weight = (1 - counter) / counter;
+
+                        ds.Points[i].weight = weight;
+                    }
+                    else {
+                        ds.Points[i].weight = 1;
+                    }
+                }
+          
+                for (int i = 0; i < db.Points.Count; i++)
+                {
+                    double r = forest[h].RunDataPoint(db.Points[i]);
+                    if (r > 0.5)
+                    {
+                        double weight = (1 - counter) / counter;
+
+                        db.Points[i].weight = weight;
+                    }
+                    else
+                    {
+                        db.Points[i].weight = 1;
+                    }
+                }
+            }
+            foreach (var d in data.Points) {
+                for (int i = 0; i < treeCount; i++) {
+                    var tree = forest[i];
+                    double r = tree.RunDataPoint(d);
+                    Console.WriteLine(d.averageSum);
+
+                    d.averageSum = d.averageSum + Math.Log(weights[i])*r;
+                }
+            }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Rohit\Documents\Visual Studio 2017\Projects\DecisionTree\DecisionTree\DecisionTree\finaldata.csv"))
+                for (int i = 0; i < data.Points.Count; i++) {
+                file.WriteLine(i + "," + data.Points[i].averageSum);
+                    
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
